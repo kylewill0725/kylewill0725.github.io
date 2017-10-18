d26d6606353ee9c08fc5bda768ee2bba8d50e076
Kyle Williams <kylewill0725@outlook.com>
2017-10-18 15:24:49 -0500

    Update

--- scripts.js
+++ scripts.js
@@ -516,7 +516,7 @@ var clockStart = function () {
 }
 
 function generateTables() {
-    finished = false;
+    finished = "loading";
     var parentDiv = document.getElementById('content');
     var currentTable = document.getElementById('currentWatching');
     var previousTable = document.getElementById('previousWatching');
@@ -599,7 +599,7 @@ function generateTables() {
                 currentRows[i].className += " addedStream";
             }
         }
-        finished = true;
+        finished = "loaded";
     });
 }
 //Run generateTables() when html finishes loading.
