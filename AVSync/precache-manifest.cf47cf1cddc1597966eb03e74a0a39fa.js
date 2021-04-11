9fcf9fa923047a64792a42e04d1eb21b673c56e2
kylewill0725 <kylewill0725@outlook.com>
2021-04-10 21:57:29 -0500

    Added avsync prototype

---
+++ AVSync/precache-manifest.cf47cf1cddc1597966eb03e74a0a39fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2445f9e10ae5a511432",
+    "url": "/css/app.8413e63a.css"
+  },
+  {
+    "revision": "8a9eec6cbcdc9b6ff8307c559a6a9a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2445f9e10ae5a511432",
+    "url": "/js/app.8ba89aac.js"
+  },
+  {
+    "revision": "dfb4ce8d7be63c126c17",
+    "url": "/js/chunk-vendors.18d0cf53.js"
+  },
+  {
+    "revision": "b5f2f714a914cc1f01badd70ab5c6dcb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
